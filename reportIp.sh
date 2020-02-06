0f4f7bd3d14dae086bb8342389c0c0b32b94fc4f
Brian Hagan <bbhagan@gmail.com>
2020-02-06 13:47:37 -0800

    Test

--- reportIp.sh
+++ reportIp.sh
@@ -3,4 +3,4 @@ IP_ADDRESS=$(hostname -I)
 IP_REPORTER_PATH=$(pwd)
 echo "IP Address: ${IP_ADDRESS}"
 echo "IP_REPORTER_PATH: ${IP_REPORTER_PATH}"
-node ${IP_REPORTER_PATH}index.js ipAddress=${IP_ADDRESS}
+node ~/ip-reporter/index.js ipAddress=${IP_ADDRESS}
