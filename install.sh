ced6544f283d7f116ddb7545ddbed689432d482b
Brian Hagan <bbhagan@gmail.com>
2020-02-06 13:43:09 -0800

    Fixed path

--- install.sh
+++ install.sh
@@ -24,5 +24,8 @@ echo "CLIENT_ID = ${CLIENT_ID}" >> .env
 
 echo "Created .env"
 
+#Setup report IP on system boot
+
+
 echo "Reporting IP"
 ./reportIp.sh ipAddress=10.10.0.256
--- reportIp.sh
+++ reportIp.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 IP_ADDRESS=$(hostname -I)
+IP_REPORTER_PATH=$(pwd)
 echo "IP Address: ${IP_ADDRESS}"
-node index.js ipAddress=${IP_ADDRESS}
+node ${IP_REPORTER_PATH}index.js ipAddress=${IP_ADDRESS}
