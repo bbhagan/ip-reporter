01817e357da4720171e696e77d5a958631cdcbaa
Brian Hagan <bbhagan@gmail.com>
2020-02-06 11:21:26 -0800

    Adding server url back into install

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ read -p "Auth key:" AUTH_KEY
 sudo apt-get install -y node
 sudo apt-get install -y npm
 
+echo "SERVER_POST_URL = http://10.10.0.162:8000/api/reportIP" > .env
 echo "AUTH_KEY = ${AUTH_KEY}" >> .env
