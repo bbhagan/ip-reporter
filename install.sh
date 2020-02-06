6b9add39345cbd4dab8b9853a0bca371f95d3dba
Brian Hagan <bbhagan@gmail.com>
2020-02-06 14:26:58 -0800

    Again

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ fi
 echo "SERVER_POST_URL = http://10.10.0.162:8000/api/reportIP" > .env
 echo "AUTH_KEY = ${AUTH_KEY}" >> .env
 echo "CLIENT_ID = ${CLIENT_ID}" >> .env
-echo "LOGS_PATH" = /home/pi/ip-reporter/logs" >> .env
+echo "LOGS_PATH = /home/pi/ip-reporter/logs" >> .env
 
 
 echo "Created .env"
