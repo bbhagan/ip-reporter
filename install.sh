bb80e83d75f20668cad46ad49c4421fd51075c7f
Brian Hagan <bbhagan@gmail.com>
2020-02-09 13:41:08 -0800

    Add logging

--- install.sh
+++ install.sh
@@ -42,13 +42,15 @@ echo "}" >> reportIp.sh
 echo "IP_ADDRESS=\$(hostname -I)" >> reportIp.sh
 
 # Output timestamp to log
-#echo "timestamp | " >> reportIp.sh
+echo "timestamp > ./logs/log.txt" >> reportIp.sh
 
 #Script to post the client IP to the server
-echo "curl --data '{\"client\": ${CLIENT_ID}, \"IP\": \"\${IP_ADDRESS}\"}' -H 'Authorization: ${AUTH_KEY}' -H 'Content-type: application/json' --silent --output ./logs/logs.txt http://${COMMAND_CONTROL_HOST}:8000/api/reportIP" >> reportIp.sh
+echo "curl --data '{\"client\": ${CLIENT_ID}, \"IP\": \"\${IP_ADDRESS}\"}' -H 'Authorization: ${AUTH_KEY}' -H 'Content-type: application/json' --silent --output ./logs/log.txt http://${COMMAND_CONTROL_HOST}:8000/api/reportIP" >> reportIp.sh
 
 #Create curl script to get WPT server IP
-echo "curl -H 'Authorization: ${AUTH_KEY}' http://${COMMAND_CONTROL_HOST}:8000/api/getServerIPCurl" >> reportIp.sh
+echo "curl -H 'Authorization: ${AUTH_KEY}' http://${COMMAND_CONTROL_HOST}:8000/api/getServerIPCurl | tee -a ./logs/log.txt" >> reportIp.sh
+
+echo "echo \"----------------------\" >> ./logs/log.txt" >> reportIp.sh
 
 #Change permissions on script 
 chmod +x reportIp.sh
