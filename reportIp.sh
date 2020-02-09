a093bb52a4ef3216006be68c426aee9da4968bfd
Brian Hagan <bbhagan@gmail.com>
2020-02-09 13:07:06 -0800

    Updated gitignore

--- reportIp.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sleep 10
-IP_ADDRESS=$(hostname -I)
-curl --data '{"client": 55, "IP": "${IP_ADDRESS}"}' -H 'Authorization: 8675309' -H 'Content-type: application/json' http://10.10.0.10:8000/api/reportIP
-echo "curl -H 'Authorization: 8675309' http://10.10.0.10:8000/api/getServerIPCurl"
